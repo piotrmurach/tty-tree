5e2eb584bcc1f6c01df051d65dfd09e28f40a3af
Piotr Murach <pmurach@gmail.com>
2017-05-01 19:29:46 +0100

    Add dsl for creating nodes on tree instance.

--- lib/tty/tree.rb
+++ lib/tty/tree.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
+require_relative 'tree/node'
 require_relative 'tree/directory_renderer'
 require_relative 'tree/number_renderer'
 require_relative 'tree/hash_walker'
@@ -11,9 +12,10 @@ module TTY
   class Tree
     # @api public
     def self.[](data)
-      self.new(data)
+      new(data)
     end
 
+    # The list of nodes in this tree.
     attr_reader :nodes
 
     # Create a Tree
@@ -21,12 +23,63 @@ module TTY
     # @param [String,Dir,Hash] data
     #
     # @api public
-    def initialize(data, options = {})
-      @data   = data.dup.freeze
+    def initialize(data = nil, options = {}, &block)
+      @data   = data ? data.dup.freeze : nil
       @walker = select_walker.new(options)
+      @nodes  = []
 
-      @walker.traverse(data)
-      @nodes = @walker.nodes
+      if @data
+        @walker.traverse(data)
+        @nodes = @walker.nodes
+      end
+
+      @nodes_stack = []
+
+      instance_eval(&block) if block_given?
+    end
+
+    # Add node to this tree.
+    #
+    # @param [Symbol,String] name
+    #   the name for the node
+    #
+    # @param [Node, LeafNode] type
+    #   the type of node to add
+    #
+    # @example
+    #   TTY::Tree.new do
+    #     node '...' do
+    #       node '...'
+    #     end
+    #   end
+    #
+    # @api public
+    def node(name, type = Node, &block)
+      parent = @nodes_stack.empty? ? Node::ROOT : @nodes_stack.last
+      level = [0, @nodes_stack.size - 1].max
+      prefix = ':pipe' * level
+      if parent.class == LeafNode
+        prefix = ':space' * level
+      end
+      node = type.new(name, parent.full_path, prefix, @nodes_stack.size)
+      @nodes << node
+
+      return unless block_given?
+
+      @nodes_stack << node
+      if block.arity.zero?
+        instance_eval(&block)
+      else
+        instance_eval(&(->(*_args) { block[node] }))
+      end
+      @nodes_stack.pop
+    end
+
+    # Add leaf node
+    #
+    # @api public
+    def leaf(name, &block)
+      node(name, LeafNode, &block)
     end
 
     # @api public
@@ -40,7 +93,7 @@ module TTY
 
     # @api private
     def select_walker
-      if @data.is_a?(Hash)
+      if @data.is_a?(Hash) || @data.nil?
         HashWalker
       else
         @data ||= Dir.pwd
--- spec/unit/new_spec.rb
+++ spec/unit/new_spec.rb
@@ -13,4 +13,73 @@ RSpec.describe TTY::Tree, '#new' do
 
     expect(tree.nodes).to_not be_empty
   end
+
+  it "provides DSL for creating tree" do
+    tree = TTY::Tree.new do
+      node 'dir1' do
+        node 'config.dat'
+        node 'dir2' do
+          node 'dir3' do
+            leaf 'file3-1.txt'
+          end
+          leaf 'file2-1.txt'
+        end
+        node 'file1-1.txt'
+        leaf 'file1-2.txt'
+      end
+    end
+
+    expect(tree.nodes).to eq([
+      TTY::Tree::Node.new('dir1', Pathname.new(''), '', 0),
+      TTY::Tree::Node.new('config.dat', Pathname.new('dir1'), '', 1),
+      TTY::Tree::Node.new('dir2', Pathname.new('dir1'), '', 1),
+      TTY::Tree::Node.new('dir3', Pathname.new('dir1/dir2'), ':pipe', 2),
+      TTY::Tree::LeafNode.new('file3-1.txt', Pathname.new('dir1/dir2/dir3'), ':pipe:pipe', 3),
+      TTY::Tree::LeafNode.new('file2-1.txt', Pathname.new('dir1/dir2'), ':pipe', 2),
+      TTY::Tree::Node.new('file1-1.txt', Pathname.new('dir1'), '', 1),
+      TTY::Tree::LeafNode.new('file1-2.txt', Pathname.new('dir1'), '', 1),
+    ])
+  end
+
+  it "provides DSL for creating orphaned tree" do
+    tree = TTY::Tree.new do
+      node 'orphan_dir' do
+        node 'a.txt'
+        node 'b.txt'
+        leaf 'data' do
+          node 'data1.bin'
+          node 'data2.sql'
+          leaf 'data3.inf'
+        end
+      end
+    end
+
+    expect(tree.nodes).to eq([
+      TTY::Tree::Node.new("orphan_dir", Pathname.new(''), '', 0),
+      TTY::Tree::Node.new("a.txt", Pathname.new('orphan_dir'), '', 1),
+      TTY::Tree::Node.new("b.txt", Pathname.new('orphan_dir'), '', 1),
+      TTY::Tree::LeafNode.new("data", Pathname.new('orphan_dir'), '',  1),
+      TTY::Tree::Node.new("data1.bin", Pathname.new('orphan_dir/data'), ':space', 2),
+      TTY::Tree::Node.new("data2.sql", Pathname.new('orphan_dir/data'), ':space', 2),
+      TTY::Tree::LeafNode.new("data3.inf", Pathname.new('orphan_dir/data'), ':space', 2)
+    ])
+  end
+
+  it "yields current node to DSL tree node" do
+    yielded = []
+
+    TTY::Tree.new do
+      node 'dir1' do |dir1|
+        yielded << dir1
+        node 'dir2' do |dir2|
+          yielded << dir2
+        end
+      end
+    end
+
+    expect(yielded).to eq([
+      TTY::Tree::Node.new("dir1", Pathname.new(''), '', 0),
+      TTY::Tree::Node.new("dir2", Pathname.new('dir1'), '', 1),
+    ])
+  end
 end
